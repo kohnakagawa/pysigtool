383ce02d27c8df2288c5f02c598e468e32ef7fa8
Koh M. Nakagawa <tsunekou1019@gmail.com>
2020-08-08 10:39:41 +0900

    fix

--- pysigtool.py
+++ pysigtool.py
@@ -7,28 +7,29 @@ import lief
 def extract_authenticode(exe: str) -> None:
     try:
         binary: lief.PE.Binary = lief.PE.parse(exe)
+        if not binary.has_signature:
+            print(f"{exe} does not have digital signatures", file=sys.stdout)
+            return
+
         size: int = binary.data_directories[
             lief.PE.DATA_DIRECTORY.CERTIFICATE_TABLE
         ].size
         offset: int = binary.data_directories[
             lief.PE.DATA_DIRECTORY.CERTIFICATE_TABLE
         ].rva
+        with open(exe, "rb") as fin:
+            raw_data = fin.read()
+        sign_entry = raw_data[offset : offset + size]
+
+        out_name = os.path.join(
+            os.path.abspath(os.path.dirname(exe)),
+            os.path.basename(exe).replace(".", "_") + ".der",
+        )
+        with open(out_name, "wb") as fout:
+            fout.write(bytes(sign_entry[8:]))  # skip header
+        print(f"saving {out_name}")
     except Exception as e:
         print(e, file=sys.stderr)
-        return
-
-    with open(exe, "rb") as fin:
-        raw_data = fin.read()
-    sign_entry = raw_data[offset : offset + size]
-
-    out_name = os.path.join(
-        os.path.abspath(os.path.dirname(exe)),
-        os.path.basename(exe).replace(".", "_") + ".der",
-    )
-    with open(out_name, "wb") as fout:
-        fout.write(bytes(sign_entry[8:]))  # skip header 8 bytes
-
-    print(f"saving {out_name}")
 
 
 def main() -> None:
